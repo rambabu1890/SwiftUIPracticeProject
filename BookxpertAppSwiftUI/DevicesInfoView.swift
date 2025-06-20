205d43915273fbd463bdcd3703ad1cd8d3630454
Rambabu <rambabu.ou1890@gmail.com>
2025-06-20 08:14:13 +0530

    Modify Device info view

--- BookxpertAppSwiftUI/DevicesInfoView.swift
+++ BookxpertAppSwiftUI/DevicesInfoView.swift
@@ -1,18 +1,70 @@
-//
-//  DevicesInfoView.swift
-//  BookxpertAppSwiftUI
-//
-//  Created by Rambabu on 25/03/25.
-//
+
 
 import SwiftUI
 
 struct DevicesInfoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            CustomHeaderView(title: "Device Info",subTile: "latest Devices",icon: "heart.fill")
+            CustomHeaderView(title: "Second Device",icon: "heart.fill")
+            GenericView(title: "Tird Device") {
+                Text("Helllo world")
+            }
+           
+                
+        }
+        Spacer()
     }
 }
 
+struct GenericView<Content:View>:View{
+    var title:String
+    var contentView:Content
+    init(title: String,@ViewBuilder contentView: () -> Content) {
+        self.title = title
+        self.contentView = contentView()
+    }
+    
+    var body: some View {
+        VStack(alignment:.leading){
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            contentView
+            RoundedRectangle(cornerRadius: 10)
+                .frame(height: 2)
+                .foregroundColor(.gray)
+        }.padding()
+            .frame(maxWidth: .infinity, alignment:.leading)
+    }
+}
+
+
 #Preview {
     DevicesInfoView()
+}
+struct CustomHeaderView : View {
+    var title:String
+    var subTile:String?
+    var icon:String?
+    var body: some View {
+        VStack(alignment:.leading){
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            if let subTile = subTile{
+                Text(subTile)
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+            }
+            if let icon = icon{
+                Image(systemName: icon)
+            }
+           
+            RoundedRectangle(cornerRadius: 10)
+                .frame(height: 2)
+                .foregroundColor(.gray)
+        }.padding()
+            .frame(maxWidth: .infinity, alignment:.leading)
+    }
 }
